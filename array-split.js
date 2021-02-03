d55c8f01fcfbfb711e43edccff500cefd7a06c31
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-04 02:11:15 +0600

    Array Split

---
+++ array-split.js
@@ -0,0 +1,14 @@
+const number = [2, 5, 7, 9, 10, 15, 32, 54, 59];
+
+//Slice
+const part = number.slice(2, 5);
+console.log(part);
+
+//Splice
+// const remove = number.splice(3, 3, 22);
+// console.log(remove);
+// console.log(number);
+
+//join
+const join = number.join(' ');
+console.log(join);
