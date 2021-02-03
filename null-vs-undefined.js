8297f0148445da1a4530be036a456d24ba8a28f9
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-03 17:58:18 +0600

    Get idea about null and undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+let pakhi;
+console.log(pakhi); 
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13, 15);
+console.log(result);
+
+function show(num1, num2){
+    console.log(num1,num2);
+}
+const pass = show(12);
+
+const premik = {name: 'romeo', age: 18, phone: 0165464};
+console.log(premik.gf);
+
+let value1 = [5, 10, 27];
+console.log(value1[5]);
+
+const value2 = undefined;
+console.log(value2);
