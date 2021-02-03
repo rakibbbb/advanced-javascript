573c8322b9066ac690a0ca85172bc641a5a2f186
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-03 21:13:28 +0600

    Learning javascript filter

--- map-filter.js
+++ map-filter.js
@@ -1,11 +1,12 @@
-// const number = [3, 5, 7, 9, 12];
-// const output = [];
-// for (let i = 0; i < number.length; i++) {
-//     const element = number[i];
-//     result = element * element;
-//     output.push(result);
-// }
-// console.log(output);
+//javascript map
+const number = [3, 5, 7, 9, 12, 18];
+const output = [];
+for (let i = 0; i < number.length; i++) {
+    const element = number[i];
+    result = element * element;
+    output.push(result);
+}
+console.log(output);
 
 const num = [5, 7, 8, 11, 15];
 const output1 = num.map(function(element){
@@ -15,3 +16,9 @@ console.log(output1);
 
 const output2 = num.map(x => x * x);
 console.log(output2);
+
+//javascript filter
+const bigger = number.filter(x => x > 7);
+console.log(bigger);
+const smaller = number.filter(x => x < 10);
+console.log(smaller);
